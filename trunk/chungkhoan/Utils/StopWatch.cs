7e3e51e4831174b822e35e3d537fecea7077c337
phamquochuy <phamquochuy@8ce7d49d-14df-6a3a-abfe-5be56979fd08>
2012-07-07 09:37:10 +0000

    - add an StopWatch utility class for measure time.

---
+++ trunk/chungkhoan/Utils/StopWatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace PhoneApp1
+{
+    public static class StopWatch
+    {
+        private static long _prevTick = DateTime.Now.Ticks;
+        private static long _currentTick = 0;
+
+        public static TimeSpan TimeSpanFromPreviousCall()
+        {
+            _currentTick = DateTime.Now.Ticks;
+            var result = new TimeSpan(_currentTick - _prevTick);
+            _prevTick = _currentTick;
+            return result;
+        }
+    }
+}
