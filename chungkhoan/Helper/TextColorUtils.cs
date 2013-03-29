9fff4b6cab184a40db4560c70df321eeafbb14ab
phamquochuy <phamquochuy@8ce7d49d-14df-6a3a-abfe-5be56979fd08>
2013-03-29 01:16:58 +0000

    - refactoring code, move GetTextColor to TextColorUtils class.

---
+++ chungkhoan/Helper/TextColorUtils.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Media;
+
+namespace PhoneApp1.Helper
+{
+    public class TextColorUtils
+    {
+        public static SolidColorBrush GetTextColor(double giaThamChieu, double gia, double tran, double san)
+        {
+            if (gia == tran)
+            {
+                return Constant.SolidColorBrush_MauTran;
+            }
+            if (gia == san)
+            {
+                return Constant.SolidColorBrush_MauSan;
+            }
+
+            if (gia > giaThamChieu)
+            {
+                return Constant.SolidColorBrush_MauTang;
+            }
+            if (gia < giaThamChieu)
+            {
+                return Constant.SolidColorBrush_MauGiam;
+            }
+
+            return Constant.SolidColorBrush_MauThamChieu;
+        }
+    }
+}
